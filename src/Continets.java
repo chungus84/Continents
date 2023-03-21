470f6db3f333d051e3530e8565bc8eec5a0b8c9c
kenichi beveridge <kenbev84@gmail.com>
2023-03-21 17:15:31 +0000

    Add switch statement

--- src/Continets.java
+++ src/Continets.java
@@ -2,5 +2,13 @@ public class Continets {
     // Program that will print the largest city in that continent based on an integer
     public static void main(String[] args) {
         int continent = 4;
+
+        switch(continent) {
+            case 1:
+                System.out.println("North America: Mexico City, Mexico");
+                break;
+            case 2:
+                System.out.println(("South America: Sao Paulo, Brazil"));
+        }
     }
 }
